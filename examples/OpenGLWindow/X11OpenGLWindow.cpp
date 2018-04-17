b25060c478608be85cdd3de218b3d3d87d97eb26
Tom Jacobs <tjacobs@expensify.com>
2018-04-17 11:39:06 -0400

    Restore

--- examples/OpenGLWindow/X11OpenGLWindow.cpp
+++ examples/OpenGLWindow/X11OpenGLWindow.cpp
@@ -416,7 +416,7 @@ void X11OpenGLWindow::enableOpenGL()
 
     // Sync to ensure any errors generated are processed.
     MyXSync( m_data->m_dpy, False );
-    if ( false && !ctxErrorOccurred && ctx )
+    if ( !ctxErrorOccurred && ctx )
       printf( "Created GL 3.0 context\n" );
     else
     {
